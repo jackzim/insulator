cffae3c537a1bd76d3d2fec7c633900b330cebb3
Jack Zimmerman <87789798+jackzim@users.noreply.github.com>
2022-10-18 13:34:12 -0500

    Updated to version 1.1

--- part_dictionaries.py
+++ part_dictionaries.py
@@ -1,6 +1,7 @@
 ## Dictionaries
 
 adders_library = {
+
 "A" : (["SUBP30B-1", 2]),
 "B" : (["CPBR20BK", 1], ["TMMX1250GY", 1]),
 "C" : (["LVGFWT200I", 1], ["TMMX1250GY", 1]),
@@ -12,33 +13,52 @@ adders_library = {
 "P1" : (["PQC200120-240", 1], ["LFL60030C3CDINR", 1], ["LFKLKR030", 3], ["GB34CL", 1], ["CH932", 1], ["CH821", 1], ["CH12X", 1], ["BT142", 1]),
 "P2" : (["PQC200120-240D", 1], ["LFL60030C3CDINR", 1], ["LFKLKR030", 3], ["GB34CL", 1], ["CH932", 1], ["CH821", 1], ["CH12X", 1], ["BT142", 1]),
 "P3" : (["PQC200120-208", 1], ["LFL60030C3CDINR", 1], ["LFKLKR030", 3], ["GB34CL", 1], ["CH932", 1], ["CH821", 1], ["CH12X", 1], ["BT142", 1]),
+"P4" : (["PQC200277-480", 1], ["LFL60030C3CDINR", 1], ["LFKLKR030", 3], ["GB34CL", 1], ["CH932", 1], ["CH821", 1], ["CH12X", 1], ["BT142", 1]),
 "P5" : (["PQC200277-480", 1], ["LFL60030C3CDINR", 1], ["LFKLKR030", 3], ["GB34CL", 1], ["CH932", 1], ["CH821", 1], ["CH12X", 1], ["BT142", 1]),
 "Q" : (["LVML2PB", 1])
+
 }
 
 w_breaker_library = {
+
     "100" : [["SI3VA6110-6HN31-0AA0", 1], ["SI3VA9243-0JB12", 2], ["SI3VA9978-0AA11", 1]],
-    "200" : [["SI3VA6110-6HN31-0AA0", 1], ["SI3VA9243-0JB12", 2], ["SI3VA9978-0AA11", 1]],
+    "200" : [["SI3VA6225-6HN31-0AA0", 1], ["SI3VA9243-0JB12", 2], ["SI3VA9978-0AA11", 1]],
     "400" : [["SI3VA6340-6HN31-0AA0", 1], ["SI3VA9473-0JJ23", 2], ["SI3VA9978-0AA11", 1]],
     "600" : [["SI3VA6460-6HN31-0AA0", 1], ["SI3VA9473-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
-    "800" : [["SI3VA6580-6HN31-0AA0", 1], ["SI3VA9673-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
-    "1000" : [["SI3VA6610-6HN31-0AA0", 1], ["SI3VA9673-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
-    "1200" : [["SIHNX3T120L"], "", ["SIASKP3"]]
+    "800" : [["SI3VA6580-6HN32-0AA0", 1], ["SI3VA9673-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
+    "1000" : [["SI3VA6610-6HN32-0AA0", 1], ["SI3VA9673-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
+    "1200" : [["SIHNX3T120L", 1], ["SIASKP3", 1]]
+
 }
 
-## 100% rated library is still under construction, need QUANTITY
-
 w_breaker_library_100 = {
+
     "100" : [["SI3VA6110-6HN31-2AA0", 1], ["SI3VA9243-0JB12", 2], ["SI3VA9978-0AA11", 1]],
-    "200" : [["SI3VA6110-6HN31-2AA0", 1], ["SI3VA9243-0JB12", 2], ["SI3VA9978-0AA11", 1]],
+    "200" : [["SI3VA6225-6HN31-2AA0", 1], ["SI3VA9243-0JB12", 2], ["SI3VA9978-0AA11", 1]],
     "400" : [["SI3VA6340-6HN31-2AA0", 1], ["SI3VA9473-0JJ23", 2], ["SI3VA9978-0AA11", 1]],
     "600" : [["SI3VA6460-6HN31-2AA0", 1], ["SI3VA9473-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
-    "800" : [["SI3VA6580-6HN31-2AA0", 1], ["SI3VA9673-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
-    "1000" : [["SI3VA6610-6HN31-2AA0", 1], ["SI3VA9673-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
+    "800" : [["SI3VA6580-6HN32-2AA0", 1], ["SI3VA9673-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
+    "1000" : [["SI3VA6610-6HN32-2AA0", 1], ["SI3VA9673-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
     "1200" : [["SIHNX3T120L", 1], ["SIASKP3", 1]]
+
+}
+
+w_breaker_library_4P = {
+
+    "100" : [["SI3VA6110-6HN41-0AA0", 1], ["SI3VA9244-0JB12", 2], ["SI3VA9978-0AA11", 1]],
+    "200" : [["SI3VA6225-6HN41-0AA0", 1], ["SI3VA9244-0JB12", 2], ["SI3VA9978-0AA11", 1]],
+    "400" : [["SI3VA6340-6HN41-0AA0", 1], ["SI3VA9474-0JJ23", 2], ["SI3VA9978-0AA11", 1]],
+    "600" : [["SI3VA6460-6HN41-0AA0", 1], ["SI3VA9474-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
+    "800" : [["SI3VA6580-6HN42-0AA0", 1], ["SI3VA9674-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
+    "1000" : [["SI3VA6610-6HN42-0AA0", 1], ["SI3VA9674-0JJ44", 2], ["SI3VA9978-0AA11", 1]],
+    "1200" : [["SIHNX3T120L", 1], ["SIASKP3", 1]]
+
 }
 
 p_745_library = {
+
+    "400" : [["SIWLS1F320UGDAXXEAN", 1], ["SIWLRP400", 1]],
+    "600" : [["SIWLS1F320UGDAXXEAN", 1], ["SIWLRP600", 1]],
     "800" : [["SIWLS1F320UGDAXXEAN", 1], ["SIWLRP800", 1]],
     "1000" : [["SIWLS1F320UGDAXXEAN", 1], ["SIWLRP1000", 1]],
     "1200" : [["SIWLS1F320UGDAXXEAN", 1], ["SIWLRP1200", 1]],
@@ -46,20 +66,25 @@ p_745_library = {
     "2000" : [["SIWLS1F320UGDAXXEAN", 1]],
     "2500" : [["SIWLL2F330WGDAXXEAN", 1], ["SIWLRP2500", 1]],
     "3000" : [["SIWLL2F330WGDAXXEAN", 1]],
+    "3200" : [["SIWLL3F3501GDAXXEAN", 1], ["SIWLRP3200", 1]],
     "4000" : [["SIWLL3F3501GDAXXEAN", 1], ["SIWLRP4000", 1]],
     "5000" : [["SIWLL3F3501GDAXXEAN"]]
+
 }
 
 p_776_library = {
-    "800" : [["S1F320UYDAXXEAN", 1], ["SIWLRP800", 1]],
-    "1000" : [["S1F320UYDAXXEAN", 1], ["SIWLRP1000", 1]],
-    "1200" : [["S1F320UYDAXXEAN", 1], ["SIWLRP1200", 1]],
-    "1600" : [["S1F320UYDAXXEAN", 1], ["SIWLRP1600", 1]],
-    "2000" : [["S1F320UYDAXXEAN", 1]],
-    "2500" : [["L2F330WYDAXXEAN", 1], ["SIWLRP2500", 1]],
-    "3000" : [["L2F330WYDAXXEAN", 1]],
-    "4000" : [["L3F3501YDAXXEAN", 1], ["SIWLRP4000", 1]],
-    "5000" : [["L3F3501YDAXXEAN", 1]]
+    
+    "800" : [["SIWLS1F320UYDAXXEAN", 1], ["SIWLRP800", 1]],
+    "1000" : [["SIWLS1F320UYDAXXEAN", 1], ["SIWLRP1000", 1]],
+    "1200" : [["SIWLS1F320UYDAXXEAN", 1], ["SIWLRP1200", 1]],
+    "1600" : [["SIWLS1F320UYDAXXEAN", 1], ["SIWLRP1600", 1]],
+    "2000" : [["SIWLS1F320UYDAXXEAN", 1]],
+    "2500" : [["SIWLL2F330WYDAXXEAN", 1], ["SIWLRP2500", 1]],
+    "3000" : [["SIWLL2F330WYDAXXEAN", 1]],
+    "3200" : [["SIWLL3F3501YDAXXEAN", 1], ["SIWLRP3200", 1]],
+    "4000" : [["SIWLL3F3501YDAXXEAN", 1], ["SIWLRP4000", 1]],
+    "5000" : [["SIWLL3F3501YDAXXEAN", 1]]
+
 }
 
 # Ensure you organize these with the bridging busbar first
@@ -78,6 +103,7 @@ tmts_library = {
 }
 
 compression_lug_library = {
+
     "100" : ["PUBLU2S10", 1],
     "200" : ["PUBLU20S4", 1],
     "300" : ["APELL30012XSH1C", 1],
@@ -98,6 +124,7 @@ compression_lug_library = {
 }
 
 w_compression_lug_locations = {
+
     "GDS-1" : 0,
     "GDS-2" : 0,
     "GDS-3" : 0,
@@ -128,6 +155,10 @@ w_compression_lug_locations = {
 
 p_compression_lugs = {
 
+    "100" : ["APELL30012XSH1C", 1],
+    "200" : ["APELL30012XSH1C", 1],
+    "400" : ["APELL30012XSH1C", 1],
+    "600" : ["APELL30012XSH1C", 1],
     "800" : ["APELL30012XSH1C", 1],
     "1000" : ["APELL30012XSH1C", 1],
     "1200" : ["APELL30012XSH1C", 1],
@@ -135,6 +166,7 @@ p_compression_lugs = {
     "2000" : ["APELL30012XSH1C", 1],
     "2500" : ["APELL30012XSH1C", 1],
     "3000" : ["APELL30012XSH1C", 1],
+    "3200" : ["APELL30012XSH1C", 1],
     "4000" : ["APELL30012XSH1C", 2],
     "5000" : ["APELL30012XSH1C", 2],
 
@@ -167,4 +199,22 @@ amount_of_breakers = {
     "TATS-1" : 1,
     "TATS-2" : 1,
     "TATS-3" : 1
+
+}
+
+temp_gen_hardwire = {
+
+    "100" : [["NSI350L2", 10], ["LDS600", 1]],
+    "200" : [["NSI350L2", 10], ["LDS600", 1]],
+    "400" : [["NSI350L2", 10], ["LDS600", 1]],
+    "800" : [["NSI350L2", 14], ["LDS600", 1]],
+    "1000" : [["NSI350L2", 18], ["LDS600", 1]],
+    "1200" : [["NSI350L2", 18], ["LDS600", 1]],
+    "1600" : [["NSI350L2", 26], ["LDS600", 1]],
+    "2000" : [["NSI350L2", 31], ["LDS600", 1]],
+    "2500" : [["NSI350L2", 39], ["LDS600", 1]],
+    "3000" : [["NSI350L2", 43], ["LDS600", 1]],
+    "3200" : [["NSI350L2", 51], ["LDS600", 1]],
+    "4000" : [["NSI350L2", 60], ["LD2S600", 1]],
+    
 }
